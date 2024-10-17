a18eb60af8520d9156c9d1dbd68e916354205418
horia-chindea <horia.chindea@gmail.com>
2024-10-17 20:02:56 +0300

    2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// JS promise 
+// JS Promisiuni (Promise) 
 function executor(resolve,reject){
     setTimeout(() => {
         const isRejected = false;
@@ -20,3 +20,47 @@ console.log(3);
 console.log(4);
 console.log(5);
 console.log(6);
+
+// async/await
+async function getUserInfo() {
+        res=0;
+        for(let i=0; i< 1000; i++){
+            for(let j=0;j<1000;j++){
+                res += i+j;
+            }
+        }
+        console.log({email: 'test@test.com'});
+}
+getUserInfo();
+console.log('operations after calling async function.');
+
+async function main() {
+    console.log("operations before calling async function");
+    await getUserInfo();
+    console.log("operations after calling async function");
+}
+main();
+
+// fetch
+const baseUrl = "https://jsonplaceholder.typicode.com/";
+const postsEndpoint = "/posts";
+
+const postsUrl = baseUrl+postsEndpoint;
+fetch(postsUrl)
+.then(response => response.json)
+.then(data => console.log(data))
+.catch(reason => console.log("error when feching data: ",reason))
+.finally((console.log("done processing request")));
+
+async function preocessPosts() {
+    const response = await fetch(postsUrl);
+    const data = await response.json();
+    console.log("data in async await: ",data);
+    const postsContainer = document.querySelector('main');
+    for(const post of data){
+        const postContainer = document.createElement('article');
+        postContainer.innerHTML = `<h3>${post.title}</h3> <p>${post.body}</p>`;
+        postsContainer.appendChild(postContainer);
+    }
+}
+preocessPosts();
